f7c077505d62d3154cf602753a21228bd8b83acd
nbwong <nicholas.b.wong@gmail.com>
2022-07-26 23:08:43 -0700

    I have updated my readme, html, and js files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
+// variables for dates
+
 var today = moment().format("l");
 $("#todays-date").text(today);
 console.log(today);
 
-var todayPlusOne = moment().add(1, 'days').format("l");
+var todayPlusOne = moment().add(1, "days").format("l");
 $("#today-plus-one").text(todayPlusOne);
 console.log(todayPlusOne);
 
-var todayPlusTwo = moment().add(2, 'days').format("l");
+var todayPlusTwo = moment().add(2, "days").format("l");
 $("#today-plus-two").text(todayPlusTwo);
 console.log(todayPlusTwo);
 
-var todayPlusThree = moment().add(3, 'days').format("l");
+var todayPlusThree = moment().add(3, "days").format("l");
 $("#today-plus-three").text(todayPlusThree);
 console.log(todayPlusThree);
 
-var todayPlusFour = moment().add(4, 'days').format("l");
+var todayPlusFour = moment().add(4, "days").format("l");
 $("#today-plus-four").text(todayPlusFour);
 console.log(todayPlusFour);
 
-var todayPlusFive = moment().add(5, 'days').format("l");
+var todayPlusFive = moment().add(5, "days").format("l");
 $("#today-plus-five").text(todayPlusFive);
 console.log(todayPlusFive);
 
@@ -26,54 +28,54 @@ console.log(todayPlusFive);
 var cityInput = document.querySelector("#city-text");
 var citySearchForm = document.querySelector("#city-form");
 var cityList = document.querySelector("#city-list");
-var cityCountSpan = document.querySelector("#city-count")
+var cityCountSpan = document.querySelector("#city-count");
 
 var cities = [];
 
 // saving the last searched city info in local storage
 function renderCities() {
-    cityList.innerHTML = "";
-    cityCountSpan.textContent = cities.length;
+  cityList.innerHTML = "";
+  cityCountSpan.textContent = cities.length;
+  
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
 
-    for (var i = 0; i < cities.length; i++) {
-        var city = cities[i];
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i);
 
-        var button = document.createElement("li");
-        li.textContent = city;
-        li.setAttribute("data-index", i);
-
-        cityList.appendChild(li);
-    }
+    cityList.appendChild(li);
+  }
 }
 
 function init() {
-    var storedCities = JSON.parse(localStorage.getItem("cities"));
+  var storedCities = JSON.parse(localStorage.getItem("cities"));
 
-    if (storedCities !== null) {
-        cities = storedCities;
-    }
+  if (storedCities !== null) {
+    cities = storedCities;
+  }
 
-    renderCities();
+  renderCities();
 }
 
 function storeCities() {
-    localStorage.setItem("cities", JSON.stringify(cities));
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
 citySearchForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var cityText = cityInput.value.trim();
+  var cityText = cityInput.value.trim();
 
-    if (cityText === "") {
-        return;
-    }
+  if (cityText === "") {
+    return;
+  }
 
-    cities.push(cityText);
-    cityInput.value = "";
+  cities.push(cityText);
+  cityInput.value = "";
 
-    storeCities();
-    renderCities();
+  storeCities();
+  renderCities();
 });
 
-init()
+init();
